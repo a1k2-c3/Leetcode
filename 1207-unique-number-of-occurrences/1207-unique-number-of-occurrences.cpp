23df1448a2ef21ef972722d797de3990b50df436
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2026-09-10 15:02:56 +0530

    Time: 1 ms (24.07%), Space: 12.1 MB (46.43%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-
-        map<int, int> mp1, mp2;
-
-        for (auto i : arr) {
-            mp1[i]++;
+        unordered_map<int,int>mp;
+        for(auto i : arr)mp[i]++;
+        unordered_map<int,int>mp2;
+        for(auto i : mp)mp2[i.second]++;
+        for(auto i : mp2){
+            if(i.second > 1)return false;
         }
-
-        for (auto i : mp1) {
-            mp2[i.second]++;
-
-            if (mp2[i.second] > 1) {
-                return false;
-            }
-        }
-
         return true;
     }
 };
